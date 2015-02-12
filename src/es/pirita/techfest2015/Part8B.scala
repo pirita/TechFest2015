25c97f590366ef7dcf172910688953d0781088b4
Ignacio <ignacio.navarro.martin@gmail.com>
2015-02-12 23:28:07 +0100

    Remove .class

--- src/es/pirita/techfest2015/Part8B.scala
+++ src/es/pirita/techfest2015/Part8B.scala
@@ -4,9 +4,9 @@ package es.pirita.techfest2015
   * @author Ignacio Navarro Martin
   * @version 1.0
   */
-object Part8B {
+object Part8B extends App{
 
-   def leapYear(year: Int): Boolean = year match {
+   def leapYear(ye: Int): Boolean = ye match {
      case year if year % 4 != 0 => false
      case year if year % 100 != 0 => true
      case year if year % 400 != 0 => false
